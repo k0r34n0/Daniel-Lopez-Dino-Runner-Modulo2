d2066a58fdedad5a3e350434c67e6b399eff0101
k0r34n0 <daniel.lopezayala@fundacion-jala.org>
2022-09-14 16:47:15 -0500

    Add more obstacles l
    adding large cactus and bird

--- dino_runner/components/obstacles/large_cactus.py
+++ dino_runner/components/obstacles/large_cactus.py
@@ -1,6 +1,7 @@
 import random
 
 from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import SCREEN_WIDTH
 
 
 class LargeCactus(Obstacle):
@@ -8,4 +9,7 @@ class LargeCactus(Obstacle):
         self.type = random.randint(0,2)
         super().__init__(image, self.type)
         self.rect.y = 325
+        screen = 355
+        screen.blit(self.image[self.obstacle_type], (self.rect.x, self.rect.y))
+
 
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,9 @@
 import pygame
 
-from dino_runner.utils.constants import SCREEN_WIDTH, SMALL_CACTUS, LARGE_CACTUS
+from dino_runner.utils.constants import BIRD, SCREEN_WIDTH, SMALL_CACTUS, LARGE_CACTUS
 from dino_runner.components.obstacles.small_cactus import SmallCactus
 from dino_runner.components.obstacles.large_cactus import LargeCactus
+from dino_runner.components.obstacles.bird import Bird
 
 
 
@@ -13,10 +14,11 @@ class ObstacleManager:
     def update(self, game):
         if len(self.obstacles) == 0:
             small_cactus = SmallCactus(SMALL_CACTUS)
-            large_cactus = LargeCactus(LARGE_CACTUS)
             self.obstacles.append(small_cactus)
+            large_cactus = LargeCactus(LARGE_CACTUS)
             self.obstacles.append(large_cactus)
-
+            bird = Bird(BIRD)
+            self.obstacles.append(bird)
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
